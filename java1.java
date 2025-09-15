e90dcbb4b4c9852842544edd3f2b5edb02ed8356
MSG2007 <msgalkina07@mail.ru>
2025-09-15 20:17:21 +0300

    Add files via upload

---
+++ java1.java
@@ -0,0 +1,10 @@
+//Создание списка на Java
+ArrayList<String> list = new ArrayList<>();
+list.add("Кошка");
+list.add("Собака");
+
+//Создание stack на Java
+MyStack<Integer> stack = new MyStack<>(10).
+stack.push(10);
+stack.push(20);
+stack.push(30);
