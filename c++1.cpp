6910f7d24209060870be573b868e017808b8b393
MSG2007 <msgalkina07@mail.ru>
2025-09-15 19:18:06 +0300

    Add files via upload

---
+++ c++1.cpp
@@ -0,0 +1,8 @@
+//Создание списка на C++ 
+list <string> cars = {'BMW','Volvo','Mazda'}
+
+//Создание stake на C++
+stack <string> stack;
+stack.push('BMW');
+stack.push('Volvo');
+stack.push('Mazda');
