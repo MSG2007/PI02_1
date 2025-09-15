8f44fe323b32e36123fa9b47b439e6c9bff60274
MSG2007 <msgalkina07@mail.ru>
2025-09-15 20:15:58 +0300

    c++1.cpp

--- c++1.cpp
+++ c++1.cpp
@@ -1,8 +1,29 @@
-//Создание списка на C++ 
-list <string> cars = {'BMW','Volvo','Mazda'}
+//Список на C++
+#include <iostream>
+#include <vector>
+using namespace std;
 
-//Создание stake на C++
-stack <string> stack;
-stack.push('BMW');
-stack.push('Volvo');
-stack.push('Mazda');
+int main() {
+    vector<int> numbers = {1,15,9,36,18};
+
+    cout << "Список чисел: ";
+    for(int i=0; i < numbers.size(); i++) {
+        cout << numbers[i] << " ";
+    }
+    return 0;
+}
+
+//Stack на C++
+#include <iostream>
+#include <stack>
+using namespace std;
+int main() {
+    stack<int> numbers;
+    numbers.push(1)
+    numbers.push(15)
+    numbers.push(9)
+    numbers.push(36)
+    numbers.push(18)
+
+    return 0;
+}
