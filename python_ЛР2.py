0075eeeb7c801f283802ecfdb9d379156a3a0606
MSG2007 <msgalkina07@mail.ru>
2025-11-16 13:52:58 +0300

    Update python_ЛР2.py

--- python_ЛР2.py
+++ python_ЛР2.py
@@ -1,1 +1,30 @@
+# Создание мультисписка (вложенного списка)
+multilist = [["яблоки", "груши"], ["молоко", "сыр"], ["хлеб", "масло"]]
+                                        
+#Создание очереди
+from queue import Queue
+q = Queue()
+q.put(1)
+q.put(2)
+q.put(3)
 
+#Создание дека
+from collections import deque
+dq = deque()
+dq.append(10)        
+dq.append(20) 
+dq.append(30) 
+
+#Создание приоритетная очереди с помощью PriorityQueue 
+from queue import PriorityQueue
+pq = PriorityQueue()
+pq.put((1, "низкий приоритет"))
+pq.put((3, "высокий приоритет"))
+pq.put((2, "средний приоритет"))
+
+#Создание приоритетной очереди с помощью heapq
+import heapq
+pq = []
+heapq.heappush(pq, (3, 'Задача 3'))
+heapq.heappush(pq, (1, 'Задача 1'))
+heapq.heappush(pq, (2, 'Задача 2'))
